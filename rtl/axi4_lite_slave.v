5d8f88ec5d486065d39d3b4593ffae6620b3b0e8
Meenal <114145036+meenalpannase@users.noreply.github.com>
2026-04-23 15:34:26 +0530

    Update axi4_lite_slave.v

--- rtl/axi4_lite_slave.v
+++ rtl/axi4_lite_slave.v
@@ -6,44 +6,44 @@ module axi4_lite_slave #(
 )(
     input  wire                       ACLK,
     input  wire                       ARESETn,
- 
+
     input  wire [ADDR_WIDTH-1:0]      AWADDR,
     input  wire [2:0]                 AWPROT,
     input  wire                       AWVALID,
     output reg                        AWREADY,
- 
+
     input  wire [DATA_WIDTH-1:0]      WDATA,
     input  wire [(DATA_WIDTH/8)-1:0]  WSTRB,
     input  wire                       WVALID,
     output reg                        WREADY,
- 
+
     output reg  [1:0]                 BRESP,
     output reg                        BVALID,
     input  wire                       BREADY,
- 
+
     input  wire [ADDR_WIDTH-1:0]      ARADDR,
     input  wire [2:0]                 ARPROT,
     input  wire                       ARVALID,
     output reg                        ARREADY,
- 
+
     output reg  [DATA_WIDTH-1:0]      RDATA,
     output reg  [1:0]                 RRESP,
     output reg                        RVALID,
     input  wire                       RREADY
 );
- 
+
 
 localparam [1:0]
     RESP_OKAY   = 2'b00,
     RESP_EXOKAY = 2'b01,
     RESP_SLVERR = 2'b10,
     RESP_DECERR = 2'b11;
- 
+
 wire [2:0] slave_prot = DEFAULT_PROT;
- 
+
 integer i;
 reg [DATA_WIDTH-1:0] reg_file [0:MEM_DEPTH-1];
- 
+
 
 localparam [2:0]
     W_IDLE = 3'd0,
@@ -51,10 +51,10 @@ localparam [2:0]
     W_ADDR = 3'd2,
     W_DATA = 3'd3,
     W_RESP = 3'd4;
- 
+
 reg [2:0] wr_state, n_wr_state;
 reg aw_done, w_done;
- 
+
 reg [ADDR_WIDTH-1:0]     wr_addr_lat;
 reg [DATA_WIDTH-1:0]     wr_data_lat;
 reg [(DATA_WIDTH/8)-1:0] wr_strb_lat;
@@ -68,47 +68,47 @@ wire wr_addr_unaligned;
 assign wr_addr_unaligned = |AWADDR[1:0];
 
 wire wr_addr_ok;
-assign wr_addr_ok = (wr_word_addr < MEM_DEPTH);
+assign wr_addr_ok = (wr_word_addr < MEM_DEPTH) && (AWADDR < (MEM_DEPTH * 4));
 
 wire wr_addr_ro;
 assign wr_addr_ro = (wr_word_addr >= 10) && (wr_word_addr <= 12);
 
 
-always @(posedge ACLK) begin
+always @(posedge ACLK or negedge ARESETn) begin
     if (!ARESETn) begin
         aw_done     <= 1'b0;
         w_done      <= 1'b0;
         wr_addr_lat <= {ADDR_WIDTH{1'b0}};
         wr_data_lat <= {DATA_WIDTH{1'b0}};
         wr_strb_lat <= {(DATA_WIDTH/8){1'b0}};
-     
+
         wr_prot_lat <= 3'b000;
     end else begin
         if (wr_state == W_RESP && BVALID && BREADY) begin
             aw_done <= 1'b0;
             w_done  <= 1'b0;
         end
-        
-        if (AWVALID && AWREADY) begin
+
+       else if (AWVALID && AWREADY) begin
             aw_done     <= 1'b1;
             wr_addr_lat <= AWADDR;
             wr_prot_lat <= AWPROT;
-           
+
         end
-        if (WVALID && WREADY) begin
+       else if (WVALID && WREADY) begin
             w_done      <= 1'b1;
             wr_data_lat <= WDATA;
             wr_strb_lat <= WSTRB;
         end
     end
 end
- 
 
-always @(posedge ACLK) begin
+
+always @(posedge ACLK or negedge ARESETn) begin
     if (!ARESETn) wr_state <= W_IDLE;
     else          wr_state <= n_wr_state;
 end
- 
+
 
 always @(*) begin
     n_wr_state = wr_state;
@@ -126,9 +126,9 @@ always @(*) begin
         default:                        n_wr_state = W_IDLE;
     endcase
 end
- 
 
-always @(posedge ACLK) begin
+
+always @(posedge ACLK or negedge ARESETn) begin
     if (!ARESETn) begin
         AWREADY <= 1'b0;
         WREADY  <= 1'b0;
@@ -144,8 +144,9 @@ always @(posedge ACLK) begin
                 BVALID  <= 1'b0;
             end
             W_BOTH: begin
-                if (AWVALID && AWREADY) AWREADY <= 1'b0;
-                if (WVALID  && WREADY)  WREADY  <= 1'b0;
+                if (aw_done || (AWVALID && AWREADY))
+                     AWREADY <= 1'b0;
+                if (w_done  || (WVALID  && WREADY ))  WREADY  <= 1'b0;
             end
             W_ADDR: begin
                 AWREADY <= 1'b1;
@@ -159,8 +160,10 @@ always @(posedge ACLK) begin
                 if (!BVALID) begin
                     if (!wr_addr_ok) begin
                         BRESP  <= RESP_DECERR;
-                    end else if (wr_addr_ro) begin
+                        reg_file[AWADDR[$clog2(MEM_DEPTH)+1:2]] <= 0;
+                    end else if (wr_addr_ro || wr_addr_unaligned) begin
                         BRESP  <= RESP_SLVERR;
+                        reg_file[AWADDR[$clog2(MEM_DEPTH)+1:2]] <= 0;
                     end else begin
                         if (wr_strb_lat[0]) reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][ 7: 0] <= wr_data_lat[ 7: 0];
                         if (wr_strb_lat[1]) reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][15: 8] <= wr_data_lat[15: 8];
@@ -170,8 +173,9 @@ always @(posedge ACLK) begin
                     end
                     BVALID <= 1'b1;
                 end
-                if (BVALID && BREADY) BVALID <= 1'b0;
+               else if (BVALID && BREADY) BVALID <= 1'b0;
                 else begin
+                    reg_file[AWADDR[$clog2(MEM_DEPTH)+1:2]] <= reg_file[AWADDR[$clog2(MEM_DEPTH)+1:2]];
                   // BVALID <= BVALID;
 //                   reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][ 7: 0] <= reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][ 7: 0];
 //                   reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][15: 8] <= reg_file[wr_addr_lat[$clog2(MEM_DEPTH)+1:2]][15: 8];
@@ -183,18 +187,17 @@ always @(posedge ACLK) begin
         endcase
     end
 end
- 
 
-localparam [1:0]
-    R_IDLE = 2'd0,
-    R_ADDR = 2'd1,
-    R_DATA = 2'd2;
- 
-reg [1:0] rd_state, n_rd_state;
+
+localparam
+    R_IDLE = 1'd0,
+    R_DATA = 1'd1;
+
+reg  rd_state, n_rd_state;
 reg [ADDR_WIDTH-1:0] rd_addr_lat;
 reg [2:0]            rd_prot_lat;
 
- 
+
 wire [$clog2(MEM_DEPTH)-1:0] rd_word_idx = rd_addr_lat[$clog2(MEM_DEPTH)+1:2];
 
 wire [$clog2(MEM_DEPTH)-1:0] rd_word_addr;
@@ -204,29 +207,29 @@ wire rd_addr_unaligned;
 assign rd_addr_unaligned = |ARADDR[1:0];
 
 wire rd_addr_ok;
-assign rd_addr_ok = (rd_word_addr < MEM_DEPTH);
+assign rd_addr_ok = (rd_word_addr < MEM_DEPTH) && (ARADDR < (MEM_DEPTH * 4));
 
 wire rd_addr_wo;
 assign rd_addr_wo = (rd_word_addr >= 13) && (rd_word_addr <= 14);
 
-always @(posedge ACLK) begin
+always @(posedge ACLK or negedge ARESETn) begin
     if (!ARESETn) rd_state <= R_IDLE;
     else          rd_state <= n_rd_state;
 end
- 
+
 
 always @(*) begin
     n_rd_state = rd_state;
+
     case (rd_state)
-        R_IDLE:  if (ARVALID)              n_rd_state = R_ADDR;
-        R_ADDR:  if (ARVALID && ARREADY)   n_rd_state = R_DATA; 
-        R_DATA:  if (RVALID  && RREADY)    n_rd_state = R_IDLE;
+        R_IDLE:  if (ARVALID && ARREADY)   n_rd_state = R_DATA;
+        R_DATA:  if (RVALID && RREADY)     n_rd_state = R_IDLE;
         default:                           n_rd_state = R_IDLE;
     endcase
 end
- 
 
-always @(posedge ACLK) begin
+
+always @(posedge ACLK or negedge ARESETn) begin
     if (!ARESETn) begin
         ARREADY     <= 1'b0;
         RVALID      <= 1'b0;
@@ -236,44 +239,42 @@ always @(posedge ACLK) begin
         rd_prot_lat <= 3'b000;
     end else begin
         case (rd_state)
- 
+
             R_IDLE: begin
-                ARREADY <= 1'b0;
-                RVALID  <= 1'b0;
-            end
- 
-            
-            R_ADDR: begin
                 ARREADY <= 1'b1;
+                RVALID  <= 1'b0;
+
                 if (ARVALID && ARREADY) begin
                     rd_addr_lat <= ARADDR;
                     rd_prot_lat <= ARPROT;
-                    
-                   
-                    ARREADY     <= 1'b0;
- 
-                    
-                    if (!rd_addr_ok) begin
-                        RDATA  <= {DATA_WIDTH{1'b0}};
-                        RRESP  <= RESP_DECERR;
-                    end else if (rd_addr_wo) begin
-                        RDATA  <= {DATA_WIDTH{1'b0}};
-                        RRESP  <= RESP_SLVERR;
-                    end else begin
-                        RDATA  <= reg_file[ARADDR[$clog2(MEM_DEPTH)+1:2]];
-                        RRESP  <= RESP_OKAY;
+
+
+                    if (!rd_addr_ok)begin
+                        RRESP <= RESP_DECERR;
+                        RDATA <= 0;
+                    end
+                    else if (rd_addr_wo || rd_addr_unaligned) begin
+                        RRESP <= RESP_SLVERR;
+                        RDATA <= 0;
+                    end
+                    else begin
+                        RRESP <= RESP_OKAY;
+                        RDATA <= reg_file[ARADDR[$clog2(MEM_DEPTH)+1:2]];
+                        
                     end
                     RVALID <= 1'b1;
+                    ARREADY <= 1'b0;
                 end
             end
- 
+
+
             R_DATA: begin
                 if (RVALID && RREADY)
                     RVALID <= 1'b0;
             end
- 
+
         endcase
     end
 end
- 
+
 endmodule
